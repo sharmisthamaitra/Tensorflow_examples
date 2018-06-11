1b1d109aaab942faa0b4e180dbb5bd7a7381d6e4
Sharmistha <sharmisthamaitra@yahoo.com>
2018-06-11 14:42:25 -0700

    added tf_homework_git to tensorflow

---
+++ tf_homework.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import numpy as np
+import random as rnd
+
+
+with tf.name_scope("Input_placeholder"):
+    a = tf.placeholder(tf.float32, shape=None, name='a_placeholder')
+    
+    mean=1
+    std=2
+    random_nbrs=np.random.normal(mean, std, 100)
+
+
+with tf.name_scope("Middle_Section"):
+    b = tf.reduce_prod(a, name="b_prod_a")
+
+    c = tf.reduce_mean(a, name="c_mean_a")
+
+    d = tf.reduce_sum(a, name="d_sum_a")
+
+    e = tf.add(c, b, name="e_sum_b_c")
+
+
+with tf.name_scope("Final_Node"):
+    f = tf.multiply(e,d, name="f_mul_d_e")
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+sess = tf.Session()
+tf.reset_default_graph() 
+
+#print(a.eval(feed_dict={a: random_nbrs}))
+#print(b.eval(feed_dict={a: random_nbrs}))
+
+sess.run(a, feed_dict={a: random_nbrs})
+sess.run(b, feed_dict={a: random_nbrs})
+sess.run(c, feed_dict={a: random_nbrs})
+sess.run(d, feed_dict={a: random_nbrs})
+sess.run(e, feed_dict={a: random_nbrs})
+sess.run(f, feed_dict={a: random_nbrs})
+
+writer = tf.summary.FileWriter('./tf_homework', sess.graph)
+#writer.close()
+#sess.close()
+
+
+
+
+
